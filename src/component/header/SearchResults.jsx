759a2ee4893ca0bbfa998667384c9cac81554feb
Roozbeh Lazemzadeh <rz.lazem@gmail.com>
2022-11-12 23:43:10 +0330

    tried fixing search

--- src/component/header/SearchResults.jsx
+++ src/component/header/SearchResults.jsx
@@ -118,39 +118,42 @@ export default function SearchResults(props) {
 					</div>
 				</form>
 				<div className="search_box_body_results">
-					{result &&
-						result.map((item) => (
-							<div className="search_box_body_results_item">
-								<img
-									src={posterImg(item.poster_path)}
-									alt={item.title}
-									className="search_box_body_results_item_img"
-								/>
-								<div className="search_box_body_results_item_details">
-									<NavLink
-										to={`/movie/${item.id}`}
-										className="search_box_body_results_item_details_title"
-									>
-										<h3 className="search_box_body_results_item_details_title_txt">
-											{`${item.title} (
-											${item.release_date.slice(0, 4)})`}
-										</h3>
-									</NavLink>
-									<span className="search_box_body_results_item_details_imdb">
-										<svg
-											xmlns="http://www.w3.org/2000/svg"
-											viewBox="0 0 448 512"
+					{result.length != 0
+						? result.map((item) => (
+								<div className="search_box_body_results_item">
+									<img
+										src={posterImg(item.poster_path)}
+										alt={item.title}
+										className="search_box_body_results_item_img"
+									/>
+									<div className="search_box_body_results_item_details">
+										<NavLink
+											to={`/movie/${item.id}`}
+											className="search_box_body_results_item_details_title"
 										>
-											<path d="M400 32H48C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48zM21.3 229.2H21c.1-.1.2-.3.3-.4zM97 319.8H64V192h33zm113.2 0h-28.7v-86.4l-11.6 86.4h-20.6l-12.2-84.5v84.5h-29V192h42.8c3.3 19.8 6 39.9 8.7 59.9l7.6-59.9h43zm11.4 0V192h24.6c17.6 0 44.7-1.6 49 20.9 1.7 7.6 1.4 16.3 1.4 24.4 0 88.5 11.1 82.6-75 82.5zm160.9-29.2c0 15.7-2.4 30.9-22.2 30.9-9 0-15.2-3-20.9-9.8l-1.9 8.1h-29.8V192h31.7v41.7c6-6.5 12-9.2 20.9-9.2 21.4 0 22.2 12.8 22.2 30.1zM265 229.9c0-9.7 1.6-16-10.3-16v83.7c12.2.3 10.3-8.7 10.3-18.4zm85.5 26.1c0-5.4 1.1-12.7-6.2-12.7-6 0-4.9 8.9-4.9 12.7 0 .6-1.1 39.6 1.1 44.7.8 1.6 2.2 2.4 3.8 2.4 7.8 0 6.2-9 6.2-14.4z"></path>
-										</svg>
-										{item.vote_average}
-									</span>
-									<p className="search_box_body_results_item_details_excerpt">
-										{item.overview}
-									</p>
+											<h3 className="search_box_body_results_item_details_title_txt">
+												{`${item.title} (
+											${item.release_date.slice(0, 4)})`}
+											</h3>
+										</NavLink>
+										<span className="search_box_body_results_item_details_imdb">
+											<svg
+												xmlns="http://www.w3.org/2000/svg"
+												viewBox="0 0 448 512"
+											>
+												<path d="M400 32H48C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48zM21.3 229.2H21c.1-.1.2-.3.3-.4zM97 319.8H64V192h33zm113.2 0h-28.7v-86.4l-11.6 86.4h-20.6l-12.2-84.5v84.5h-29V192h42.8c3.3 19.8 6 39.9 8.7 59.9l7.6-59.9h43zm11.4 0V192h24.6c17.6 0 44.7-1.6 49 20.9 1.7 7.6 1.4 16.3 1.4 24.4 0 88.5 11.1 82.6-75 82.5zm160.9-29.2c0 15.7-2.4 30.9-22.2 30.9-9 0-15.2-3-20.9-9.8l-1.9 8.1h-29.8V192h31.7v41.7c6-6.5 12-9.2 20.9-9.2 21.4 0 22.2 12.8 22.2 30.1zM265 229.9c0-9.7 1.6-16-10.3-16v83.7c12.2.3 10.3-8.7 10.3-18.4zm85.5 26.1c0-5.4 1.1-12.7-6.2-12.7-6 0-4.9 8.9-4.9 12.7 0 .6-1.1 39.6 1.1 44.7.8 1.6 2.2 2.4 3.8 2.4 7.8 0 6.2-9 6.2-14.4z"></path>
+											</svg>
+											{item.vote_average}
+										</span>
+										<p className="search_box_body_results_item_details_excerpt">
+											{item.overview}
+										</p>
+									</div>
 								</div>
-							</div>
-						))}
+						  ))
+						: inputValue.length != "" && (
+								<h6 className="no_results">No results not found!</h6>
+						  )}
 				</div>
 			</div>
 		</div>
